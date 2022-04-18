0baa132b35c0d8be8676abea4a62e7e9c5f0cc89
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-04-19 00:06:03 +0100

    Update rot13ed_string.c

--- rot13ed_string.c
+++ rot13ed_string.c
@@ -1,1 +1,33 @@
-binbash
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rot13 - function converts a string to rot13
+* @list: string to be converted
+* Return: Count of coverted string
+*/
+int rot13(va_list list)
+{
+	int i, j;
+	char *str;
+	char str1[] = "abcdefjhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char str2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		return (-1);
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == str1[j])
+			{
+				_putchar(str2[j]);
+				break;
+			}
+		}
+		if (i == 52)
+			_putchar(str[i]);
+	}
+	return (i);
+}
